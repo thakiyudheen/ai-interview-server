0e9fd6869f89f12881bd500b003d973b6d9efb5c
thakiyudheen <thakeyudheen525@gmail.com>
2025-08-18 19:19:36 +0530

    mongodbupdated

--- src/config/db-connect.ts
+++ src/config/db-connect.ts
@@ -3,12 +3,22 @@ import mongoose from "mongoose";
 const MONGO_URL =
   process.env.MONGO_URI || "mongodb://127.0.0.1:27017/bg-remover";
 
+let isConnected: boolean = false;
+
 export const connectToDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
-    await mongoose.connect(MONGO_URL);
+    const db = await mongoose.connect(MONGO_URL, {
+      bufferCommands: false,
+    });
+
+    isConnected = db.connections[0].readyState === 1;
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
-    process.exit(1);
+    throw error;
   }
 };
